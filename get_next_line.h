6cdd1b881a45c38b3064c9e21803f7c8610bc74b
Linux - Thuis <rutgermeuzelaar@gmail.com>
2024-02-11 21:25:35 +0100

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rmeuzela <rmeuzela@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/23 13:05:33 by rmeuzela      #+#    #+#                 */
+/*   Updated: 2024/01/11 22:44:16 by rmeuzela      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+# if BUFFER_SIZE <= 0
+#  error "BUFFER_SIZE needs to be greater than 0."
+# endif
+# include <stdlib.h>
+# include <stdio.h>
+
+int		set_buffer(char **buffer, int fd);
+char	*create_line(char *line, char **buffer);
+char	*divide_remainder(char **buffer, int chars_to_newline);
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *s);
+int		get_chars_to_newline(char *string);
+char	*ft_strjoin(char *line, char *buffer);
+char	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
---
+++ get_next_line_bonus.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_bonus.c                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rmeuzela <rmeuzela@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/11/23 13:09:45 by rmeuzela      #+#    #+#                 */
+/*   Updated: 2024/01/12 15:16:28 by rmeuzela      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int	set_buffer(char **buffer, int fd)
+{
+	int		bytes_read;
+
+	bytes_read = 1;
+	if (*buffer == NULL)
+	{
+		*buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		if (*buffer == NULL)
+			return (-1);
+		bytes_read = read(fd, *buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			free(*buffer);
+			*buffer = NULL;
+		}
+	}
+	return (bytes_read);
+}
+
+char	*create_line(char *line, char **buffer)
+{
+	int		chars_to_newline;
+	char	*string_to_append;
+
+	chars_to_newline = get_chars_to_newline(*buffer);
+	if (chars_to_newline != -1 && chars_to_newline != BUFFER_SIZE - 1)
+	{
+		string_to_append = divide_remainder(buffer, chars_to_newline);
+		if (string_to_append == NULL)
+			return (free(line), NULL);
+		line = ft_strjoin(line, string_to_append);
+		free(string_to_append);
+	}
+	else
+	{
+		line = ft_strjoin(line, *buffer);
+		free(*buffer);
+		*buffer = NULL;
+	}
+	if (line == NULL)
+	{
+		free(*buffer);
+		*buffer = NULL;
+		return (NULL);
+	}
+	return (line);
+}
+
+char	*divide_remainder(char **buffer, int chars_to_newline)
+{
+	char	*temp_buffer;
+	char	*string_to_append;
+
+	temp_buffer = NULL;
+	temp_buffer = ft_strjoin(temp_buffer, *buffer);
+	free(*buffer);
+	*buffer = NULL;
+	if (temp_buffer == NULL)
+		return (NULL);
+	string_to_append = ft_substr(temp_buffer, 0, chars_to_newline + 1);
+	if (string_to_append == NULL)
+		return (free(temp_buffer), NULL);
+	*buffer = ft_substr(temp_buffer, chars_to_newline + 1, \
+	BUFFER_SIZE - chars_to_newline);
+	if (*buffer == NULL)
+		return (free(temp_buffer), free(string_to_append), NULL);
+	if (ft_strlen(*buffer) == 0)
+	{
+		free(*buffer);
+		*buffer = NULL;
+	}
+	free(temp_buffer);
+	return (string_to_append);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[FILE_DESCRIPTOR_MAX];
+	char		*line;
+	int			bytes_read;
+
+	line = NULL;
+	if (fd < 0 || fd > FILE_DESCRIPTOR_MAX)
+		return (NULL);
+	while (get_chars_to_newline(line) == -1)
+	{
+		bytes_read = set_buffer(&buffer[fd], fd);
+		if (bytes_read == 0)
+			break ;
+		if (bytes_read < 0)
+			return (free(line), NULL);
+		line = create_line(line, &buffer[fd]);
+		if (line == NULL)
+			return (NULL);
+	}
+	return (line);
+}
+
+// int main(void)
+// {
+// 	char	*return_value;
+// 	int 	fd_hond = open("test_1", O_RDONLY);
+// 	int		fd_kaas = open("test_2", O_RDONLY);
+// 	bool	loop_switch;
+// 	int		i;
+// 	int		fd;
+
+// 	i = 0;
+// 	fd = 0;
+// 	loop_switch = true;
+// 	while (loop_switch)
+// 	{
+// 		if (i % 2 == 0)
+// 			fd = fd_hond;
+// 		else
+// 			fd = fd_kaas;
+// 		return_value = get_next_line(fd);
+// 		if (return_value == NULL)
+// 			loop_switch = false;
+// 		printf(">%s<\n", return_value);
+// 		free(return_value);
+// 		i++;
+// 	}
+// 	return (0);
+// } 
